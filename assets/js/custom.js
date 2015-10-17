7862a5bd944f463688551df0fb6a3eb9bbb731ce
Jonathan Dreyer <jonathan.dreyer@cleanenergie.ch>
2015-10-17 22:50:49 +0200

    Add scroll-up button & new version portfolio bachelor

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -39,6 +39,32 @@
 		});
 		
 		/* ---------------------------------------------- /*
+		 * Animated scrolling / Scroll Up
+		/* ---------------------------------------------- */
+
+		$('a[href*=#]').bind("click", function(e){
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('.scroll-up').fadeIn();
+			} else {
+				$('.scroll-up').fadeOut();
+			}
+		});
+
+		$(document).on('click','.navbar-collapse.in',function(e) {
+			if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+				$(this).collapse('hide');
+			}
+		});
+		
+		/* ---------------------------------------------- /*
 		 * Size
 		/* ---------------------------------------------- */
 		
